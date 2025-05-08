61e07ea12ab3622e027f8af76c7d0cd2cb615e41
sutouch08 <sutouch08@gmail.com>
2025-05-08 17:28:15 +0700

    fix bug

--- application/controllers/rest/V1/Stock_zone.php
+++ application/controllers/rest/V1/Stock_zone.php
@@ -68,7 +68,7 @@ class Stock_zone extends REST_Controller
       $this->response($arr, 400);
     }
   }
-  
+
   //---- for check stock
   public function getStockZone_get()
   {
@@ -110,6 +110,73 @@ class Stock_zone extends REST_Controller
           'status' => TRUE,
           'data' => $result,
           'count' => count($result),
+          'error' => 'success'
+        );
+
+        $this->response($arr, 200);
+      }
+      else
+      {
+        $arr = array(
+          'status' => FALSE,
+          'error' => 'ไม่พบโซนในระบบ'
+        );
+
+        $this->response($arr, 200);
+      }
+    }
+    else
+    {
+      $arr = array(
+        'status' => FALSE,
+        'error' =>"Missing required parameter 'zone code'"
+      );
+
+      $this->response($arr, 400);
+    }
+  }
+
+  //---- for check stock
+  public function getStockZone_post()
+  {
+    //--- Get raw post data
+    $json = file_get_contents("php://input");
+
+    $data = json_decode($json);
+
+    if(empty($data) OR empty($data->zone_code))
+    {
+      $arr = array(
+        'status' => FALSE,
+        'error' => 'empty data'
+      );
+
+      $this->response($arr, 400);
+    }
+
+    if( ! empty($data->zone_code))
+    {
+      $zone = $this->zone_model->get($data->zone_code);
+
+      if( ! empty($zone))
+      {
+        $result = NULL;
+
+        if($zone->is_consignment)
+        {
+          $this->cn = $this->load->database('cn', TRUE);
+          $result = $this->stock_model->get_all_stock_consignment_zone($zone->code);
+        }
+        else
+        {
+          $this->ms = $this->load->database('ms', TRUE);
+          $result = $this->stock_model->get_all_stock_in_zone($zone->code);
+        }
+
+        $arr = array(
+          'status' => TRUE,
+          'data' => $result,
+          'count' => empty($result) ? 0 : count($result),
           'error' => 'success'
         );
 
