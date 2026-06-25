4e2a9c0ff736188530a7e6474852fcb6d20c0e74
sutouch08 <sutouch08@gmail.com>
2026-06-25 19:21:40 +0700

    auto_send_to_sap

--- application/controllers/auto/Auto_send_delivery.php
+++ application/controllers/auto/Auto_send_delivery.php
@@ -100,5 +100,142 @@ class Auto_send_delivery extends PS_Controller
     return NULL;    
   }
 
+
+  public function import_order()
+  {
+    ini_set('max_execution_time', 1200);
+    ini_set('memory_limit', '1000M');
+
+    $sc = TRUE;
+
+    $file = isset($_FILES['uploadFile']) ? $_FILES['uploadFile'] : FALSE;
+    $path = $this->config->item('upload_path') . 'import_files/';
+    $file  = 'uploadFile';
+    $config = array(   // initial config for upload class
+      "allowed_types" => "xlsx",
+      "upload_path" => $path,
+      "file_name"  => "import_file",
+      "max_size" => 5120,
+      "overwrite" => TRUE
+    );
+
+    $this->load->library("upload", $config);
+
+    if (! $this->upload->do_upload($file))
+    {
+      $sc = FALSE;
+      $this->error = $this->upload->display_errors();
+    }
+    else
+    {
+      $info = $this->upload->data();
+      $this->load->library('excel');
+      /// read file
+      $excel = PHPExcel_IOFactory::load($info['full_path']);
+      //get only the Cell Collection
+      $collection  = $excel->getActiveSheet()->toArray(NULL, TRUE, TRUE, TRUE);
+
+      if (! empty($collection))
+      {
+        $i = 1;
+        $j = 0;
+        $ds = [];
+        $ro = [];
+
+        foreach ($collection as $rs)
+        {
+          if ($i > 1)
+          {
+            $j++;
+            $ro[] = array('code' => trim($rs['A']));
+
+            if ($j == 1000)
+            {
+              $j = 0;
+              $ds[] = $ro;
+              $ro = [];
+            }
+          }
+
+          $i++;
+        }
+
+        $ds[] = $ro;
+
+        if (! empty($ds))
+        {
+          foreach ($ds as $rows)
+          {
+            if (! $this->insert($rows))
+            {
+              $sc = FALSE;
+              $this->error = "Cannot insert data";
+            }
+          }
+        }
+      }
+      else
+      {
+        $sc = FALSE;
+        $this->error = "Cannot get data from import file : empty data collection";
+      }
+    }
+
+    $this->_response($sc);
+  }
+
+
+
+  public function insert(array $ds = array())
+  {
+    if (! empty($ds))
+    {
+      return $this->db->insert_batch('auto_send_to_sap_order', $ds);
+    }
+
+    return FALSE;
+  }
+
+
+  public function clear_data()
+  {
+    $sc = TRUE;
+
+    $qr = "TRUNCATE TABLE auto_send_to_sap_order";
+
+    if (! $this->db->query($qr))
+    {
+      $sc = FALSE;
+      $this->error = "Failed to clear data";
+    }
+
+    $this->_response($sc);
+  }
+
+  
+  public function change_order_limit()
+  {
+    $sc = TRUE;
+    $limit = $this->input->post('limit');
+
+    if ($limit > 0)
+    {
+      $this->load->model('setting/config_model');
+
+      if (! $this->config_model->update('AUTO_CHANGE_STATE_LIMIT', $limit))
+      {
+        $sc = FALSE;
+        $this->error = "Failed to update config value";
+      }
+    }
+    else
+    {
+      $sc = FALSE;
+      $this->error = "จำนวนต้องมากกว่า 0";
+    }
+
+    $this->_response($sc);
+  }
+
 } //--- end class
  ?>
--- application/views/auto/auto_send_to_sap.php
+++ application/views/auto/auto_send_to_sap.php
@@ -5,19 +5,21 @@
 	</div>
 	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 padding-5 text-right">
 		<button type="button" class="btn btn-white btn-info top-btn" onclick="refresh()"><i class="fa fa-refresh"></i> Refresh</button>
-		<button type="button" class="btn btn-white btn-success btn-50 top-btn" onclick="startExport()"><i class="fa fa-bolt"></i> Start Export</button>
+		<button type="button" class="btn btn-white btn-primary top-btn" onclick="getUploadFile()"><i class="fa fa-upload"></i> &nbsp; Import Order</button>
+		<button type="button" class="btn btn-white btn-warning top-btn" onclick="clearAllData()"><i class="fa fa-times"></i> &nbsp; Clear All Data</button>
+		
 	</div>
 </div>
 <hr />
 <div class="row">
 	<form id="searchForm" method="post" action="<?php echo $this->home; ?>">
-		<div class="col-lg-2-harf col-md-2-harf col-sm-3-harf col-xs-4 padding-5">
+		<div class="col-lg-2-harf col-md-2-harf col-sm-3-harf col-xs-6 padding-5">
 			<div class="input-group">
 				<span class="input-group-addon">รหัส</span>
 				<input type="text" class="form-control input-sm" id="code" name="code" placeholder="Order Code" value="<?php echo $code; ?>" />
 			</div>
 		</div>
-		<div class="col-lg-2 col-md-2-harf col-sm-3-harf col-xs-4 padding-5">
+		<div class="col-lg-2 col-md-2-harf col-sm-3-harf col-xs-6 padding-5">
 			<div class="input-group">
 				<span class="input-group-addon">สถานะ</span>
 				<select class="form-control input-sm" id="status" name="status">
@@ -28,13 +30,24 @@
 				</select>
 			</div>
 		</div>
-		<div class="col-lg-1 col-md-1-harf col-sm-1-harf col-xs-3 padding-5">
+		<div class="divider visible-xs"></div>
+		<div class="col-lg-1 col-md-1-harf col-sm-1-harf col-xs-6 padding-5">
 			<button type="submit" class="btn btn-xs btn-primary btn-block" style="height:30px;">Search</button>
 		</div>
-		<div class="col-lg-1 col-md-1-harf col-sm-1-harf col-xs-3 padding-5">
+		<div class="col-lg-1 col-md-1-harf col-sm-1-harf col-xs-6 padding-5">
 			<button type="button" class="btn btn-xs btn-warning btn-block" onclick="clearOption()" style="height:30px;">Clear</button>
 		</div>
 	</form>
+	<div class="divider visible-xs visible-sm"></div>
+	<div class="col-lg-2 col-md-2-harf col-sm-4 col-xs-6 padding-5">
+		<div class="input-group">
+			<span class="input-group-addon">จำนวน</span>
+			<input type="number" class="form-control input-sm text-center" id="order-limit" onchange="changeOrderLimit()" value="<?php echo $limit; ?>" />
+		</div>
+	</div>
+	<div class="col-lg-1-harf col-md-1-harf col-sm-2 col-xs-6 padding-5">
+		<button type="button" class="btn btn-xs btn-success btn-block" onclick="startExport()" style="height:30px;">Start Export</button>
+	</div>
 </div>
 <hr />
 <div class="row" id="result">
@@ -74,8 +87,38 @@
 	</div>
 </div>
 
+<div class="modal fade" id="upload-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog" style="width:600px; max-width:95vw;">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title">Import File</h4>
+			</div>
+			<div class="modal-body">
+				<form id="upload-form" name="upload-form" method="post" enctype="multipart/form-data">
+					<div class="row">
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+							<div class="input-group width-100">
+								<input type="text" class="form-control" id="show-file-name" placeholder="กรุณาเลือกไฟล์ Excel" readonly />
+								<span class="input-group-btn">
+									<button type="button" class="btn btn-white btn-default" onclick="getFile()">เลือกไฟล์</button>
+								</span>
+							</div>
+						</div>
+					</div>
+					<input type="file" class="hide" name="uploadFile" id="uploadFile" accept=".xlsx" />
+				</form>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-sm btn-default btn-100" onclick="closeModal('upload-modal')">ยกเลิก</button>
+				<button type="button" class="btn btn-sm btn-primary btn-100" onclick="uploadfile()">นำเข้า</button>
+			</div>
+		</div>
+	</div>
+</div>
+
 <script>
-	var HOME = BASE_URL + 'auto/auto_send_delivery';
+	var HOME = BASE_URL + 'auto/auto_send_delivery/';
 
 	var finished = false;
 	var max = 0;
@@ -97,8 +140,7 @@
 
 		if (orders.length > 0 && max > 0) {
 			do_export(0);
-		}
-		else {
+		} else {
 			load_out();
 		}
 	}
@@ -161,6 +203,146 @@
 			},
 			success: function(rs) {
 				console.log(rs);
+			}
+		})
+	}
+
+	function getUploadFile() {
+		$('#upload-modal').modal('show');
+	}
+
+
+	function getFile() {
+		$('#uploadFile').click();
+	}
+
+
+	$("#uploadFile").change(function() {
+		if ($(this).val() != '') {
+			var file = this.files[0];
+			var name = file.name;
+			var type = file.type;
+			var size = file.size;
+
+			if (size > 5000000) {
+				swal("ขนาดไฟล์ใหญ่เกินไป", "ไฟล์แนบต้องมีขนาดไม่เกิน 5 MB", "error");
+				$(this).val('');
+				return false;
+			}
+			//readURL(this);
+			$('#show-file-name').val(name);
+		}
+	});
+
+
+	function uploadfile() {
+		$('#upload-modal').modal('hide');
+
+		var file = $("#uploadFile")[0].files[0];
+		var fd = new FormData();
+		fd.append('uploadFile', $('input[type=file]')[0].files[0]);
+		if (file !== '') {
+			load_in();
+
+			$.ajax({
+				url: BASE_URL + 'auto/auto_change_state/import_order',
+				type: "POST",
+				cache: false,
+				data: fd,
+				processData: false,
+				contentType: false,
+				success: function(rs) {
+					load_out();
+
+					if (rs.trim() === 'success') {
+						swal({
+							title: 'นำเข้าเรียบร้อยแล้ว',
+							type: 'success',
+							html: true,
+							timer: 1000
+						});
+
+						setTimeout(() => {
+							refresh();
+						}, 1200);
+					} else {
+						showError(rs);
+					}
+				},
+				error: function(rs) {
+					showError(rs);
+				}
+			});
+		}
+	}
+
+
+	function clearAllData() {
+		swal({
+			title: 'Clear Data',
+			text: 'Clear All Data, Do you want to process this operation ?',
+			type: 'warning',
+			html: true,
+			showCancelButton: true,
+			cancelButtonText: 'No',
+			confirmButtonText: 'Yes',
+			closeOnConfirm: true
+		}, function() {
+			load_in();
+
+			$.ajax({
+				url: HOME + 'clear_data',
+				type: 'GET',
+				cache: false,
+				success: function(rs) {
+					load_out();
+
+					if (rs.trim() === 'success') {
+						swal({
+							title: 'success',
+							type: 'success',
+							timer: 1000,
+						});
+
+						setTimeout(() => {
+							refresh();
+						}, 1200);
+					} else {
+						showError(rs);
+					}
+				},
+				error: function(rs) {
+					showError(rs);
+				}
+			})
+		})
+	}
+
+
+	function changeOrderLimit() {
+		let limit = parseDefault(parseInt($('#order-limit').val()), 0);
+
+		if (limit <= 0) {
+			swal("จำนวนต้องมากกว่า 0");
+			return false;
+		}
+
+		$.ajax({
+			url: HOME + 'change_order_limit',
+			type: 'POST',
+			cache: false,
+			data: {
+				'limit': limit
+			},
+			success: function(rs) {
+				if (rs.trim() === 'success') {
+					refresh();
+				} else {
+					showError(rs);
+				}
+			},
+			error: function(rs) {
+				showError(rs);
 			}
 		})
 	}
